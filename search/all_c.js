191e2bd965eebdebdd2a2c1a208baf96cf1c4a26
RADEON73 <RADEON73@users.noreply.github.com>
2025-04-27 07:55:30 +0000

    deploy: 8604a014a412d5ec638e30d4c5f599fcc41bde55

--- search/all_c.js
+++ search/all_c.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['parse_0',['parse',['../classBulkProcessor.html#aaa6df43cb9ed53ebbf61f7adbe6ea163',1,'BulkProcessor']]],
+  ['parse_0',['parse',['../classBulkProcessor.html#a567d5d3e67d8937cb16afe58fa43003d',1,'BulkProcessor']]],
   ['process_5fcommands_5ffrom_5fstream_1',['process_commands_from_stream',['../ProcessorCommands_8h.html#a961c2ba040f3f6f90a39c4619cdc4bbb',1,'ProcessorCommands.h']]],
   ['processorcommand_2',['processorcommand',['../classProcessorCommand.html',1,'ProcessorCommand'],['../classProcessorCommand.html#a6f1cc6815b7631167cd5b0ee747f2ece',1,'ProcessorCommand::ProcessorCommand()'],['../classConnectCommand.html#a6f1cc6815b7631167cd5b0ee747f2ece',1,'ConnectCommand::ProcessorCommand()'],['../classReceiveCommand.html#a6f1cc6815b7631167cd5b0ee747f2ece',1,'ReceiveCommand::ProcessorCommand()'],['../classDataCommand.html#a6f1cc6815b7631167cd5b0ee747f2ece',1,'DataCommand::ProcessorCommand()'],['../classDisconnectCommand.html#a6f1cc6815b7631167cd5b0ee747f2ece',1,'DisconnectCommand::ProcessorCommand()'],['../classListCommand.html#a6f1cc6815b7631167cd5b0ee747f2ece',1,'ListCommand::ProcessorCommand()'],['../classExitCommand.html#a6f1cc6815b7631167cd5b0ee747f2ece',1,'ExitCommand::ProcessorCommand()']]],
   ['processorcommands_2eh_3',['ProcessorCommands.h',['../ProcessorCommands_8h.html',1,'']]],
--- search/functions_c.js
+++ search/functions_c.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['parse_0',['parse',['../classBulkProcessor.html#aaa6df43cb9ed53ebbf61f7adbe6ea163',1,'BulkProcessor']]],
+  ['parse_0',['parse',['../classBulkProcessor.html#a567d5d3e67d8937cb16afe58fa43003d',1,'BulkProcessor']]],
   ['process_5fcommands_5ffrom_5fstream_1',['process_commands_from_stream',['../ProcessorCommands_8h.html#a961c2ba040f3f6f90a39c4619cdc4bbb',1,'ProcessorCommands.h']]],
   ['processorcommand_2',['processorcommand',['../classProcessorCommand.html#a6f1cc6815b7631167cd5b0ee747f2ece',1,'ProcessorCommand::ProcessorCommand()'],['../classConnectCommand.html#a6f1cc6815b7631167cd5b0ee747f2ece',1,'ConnectCommand::ProcessorCommand()'],['../classReceiveCommand.html#a6f1cc6815b7631167cd5b0ee747f2ece',1,'ReceiveCommand::ProcessorCommand()'],['../classDataCommand.html#a6f1cc6815b7631167cd5b0ee747f2ece',1,'DataCommand::ProcessorCommand()'],['../classDisconnectCommand.html#a6f1cc6815b7631167cd5b0ee747f2ece',1,'DisconnectCommand::ProcessorCommand()'],['../classListCommand.html#a6f1cc6815b7631167cd5b0ee747f2ece',1,'ListCommand::ProcessorCommand()'],['../classExitCommand.html#a6f1cc6815b7631167cd5b0ee747f2ece',1,'ExitCommand::ProcessorCommand()']]],
   ['push_3',['push',['../classThreadSafeQueue.html#a402ba25c8544045726d5644568c30794',1,'ThreadSafeQueue']]]
